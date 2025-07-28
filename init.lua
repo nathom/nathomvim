4d65e6c1d527bfc1c4426108fca78f8bbb377535
Nathan Thomas <nathanthomas707@gmail.com>
2025-07-28 13:44:10 -0700

    Remove old telescope branch

--- init.lua
+++ init.lua
@@ -424,7 +424,6 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
 	{ -- Fuzzy Finder (files, lsp, etc)
 		"nvim-telescope/telescope.nvim",
 		event = "VimEnter",
-		branch = "0.1.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ -- If encountering errors, see telescope-fzf-native README for installation instructions
