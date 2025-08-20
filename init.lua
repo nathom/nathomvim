b963020e41ef4fb9548c49fa35b91774bb9e8941
Nathan Thomas <nathanthomas707@gmail.com>
2025-08-20 11:09:41 -0700

    Reluctantly add hls back

--- init.lua
+++ init.lua
@@ -810,7 +810,7 @@ require("nixCatsUtils.lazyCat").setup(nixCats.pawsible({ "allPlugins", "start", 
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			-- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
 			local servers = {}
-			-- servers.hls = {} -- this has major performance issues :(
+			servers.hls = {} -- this has major performance issues :(
 			-- servers.clangd = {},
 			-- servers.gopls = {},
 			-- servers.pyright = {}
